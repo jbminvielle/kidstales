448dd2234bbea92e0641af5aa426982152b183f6
DrJaxx <durufe@gmail.com>
2013-02-20 20:33:12 +0100

    Intervenant ajoute

---
+++ app/Controllers/Inter_controller.php
@@ -0,0 +1,53 @@
+<?php
+class App_controller{
+ 
+ function __construct(){
+  
+ }
+ 
+ function home(){
+    // $id=F3::get('PARAMS.id');
+    // #récupération de la destination courante
+    // $App=new App();
+    // $location=$App->locationDetails($id);
+    // if(!$location){
+    //   F3::error('404');
+    //   return;
+    // }
+    // F3::set('location',$location);
+    
+    // if(F3::get('AJAX')){
+    //   $ajax['coords']['lat']=$location->lat;
+    //   $ajax['coords']['lng']=$location->lng;
+    //   $pictures=App::instance()->locationPictures($location->id);
+    //   $ajax['pictures']=array_map(function($item){return array('image'=>$item->src);},$pictures);
+    //   echo json_encode($ajax);
+    //   return;
+    // }
+
+    
+    // $next=$App->getNext($location->id);
+    // $prev=$App->getPrev($location->id);
+    
+   
+    // $linkNext=$next?$next[0]['id'].'-'.$next[0]['title']:'';
+    // $linkPrev=$prev?$prev[0]['id'].'-'.$prev[0]['title']:'';
+    
+    // F3::set('next',$linkNext);
+    // F3::set('prev',$linkPrev);
+    
+    
+    echo Views::instance()->render('index.html');
+ }
+ 
+ 
+  function doc(){
+    echo Views::instance()->render('userref.html');
+  }
+  
+ 
+ function __destruct(){
+
+ } 
+}
+?>
---
+++ app/Models/Intervenant.php
@@ -0,0 +1,41 @@
+<?php
+class Intervenant extends Prefab{
+  
+  function __construct(){
+      F3::set('dB',new DB\SQL(
+        'mysql:host='.F3::get('db_host').';port=3306;dbname='.F3::get('db_server'),
+        F3::get('db_user'),
+        F3::get('db_pw')));
+  }
+  
+  function sortStoriesByLocation($attr){
+    $storiesDB=new DB\SQL\Mapper(F3::get('dB'),'stories');
+    return $storiesDB->find(array('votes>=?',20,'location=?',$attr),array('order'=>'note','limit'=>20));
+
+  }
+  
+  function locationPictures($idLocation){
+    $pictures=new DB\SQL\Mapper(F3::get('dB'),'pictures');
+    return $pictures->find(array('idLocation=?',$idLocation));
+  }
+  
+  function getNext($id){  
+    return F3::get('dB')->exec("select id, title from location where id =(select min(id) from location where id > ".$id.")");
+  }
+  
+  function getPrev($id){
+    return F3::get('dB')->exec("select id, title from location where id =(select max(id) from location where id < ".$id.")");
+  }
+  
+
+  function __destruct(){
+
+  }
+}
+
+
+
+
+
+
+?>
