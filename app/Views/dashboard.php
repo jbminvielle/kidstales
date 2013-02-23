a9b066b3954b861864cb78d11b9addd3141604c7
Jean-Baptiste Minvielle <jean.baptiste.minvielle@gmail.com>
2013-02-23 16:49:31 +0100

    dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -11,7 +11,26 @@
 
 					<p>
 					<!--début de la boucle-->
-							
+						
+						<div class="dashboard_element">
+							<span class="datetime">
+								Il y a 5 heures
+							</span>
+
+							<span class="picture">
+							</span>
+
+							<span class="line1">
+								<span class="author">Julien</span> 
+								(<span class="lieu">Chateau de Vincennes</span>).
+							</span>
+
+							<span class="line2">
+								<span class="note">4/5</span> -
+								<span class="medias">Commentaire, photos</span>
+								<span class="verify">[vérifier]</span>
+							</span>
+						</div>
 						
 					<!--fin de la boucle-->
 					</p>
@@ -19,6 +38,7 @@
 			</section>
 		</div>
 
+		<!-- todo : afficher dans la sidebar la liste des enfants-->
 		<?php require "partials/sidebar.php"; ?>
 
 <?php
