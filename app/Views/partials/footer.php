a3290866d11bcf069bd684d8210d65e2873bd431
Jean-Baptiste Minvielle <jean.baptiste.minvielle@gmail.com>
2013-03-03 13:02:40 +0100

    correction du footer et du form

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,9 +1,12 @@
 			</div> <!-- ajax content -->
-			<footer>
-				<p>Réalisation : étudiants d’Hétic, P2015 | <strong>Dispositions concernant la sécurité des enfants</strong> | <a href="http://facebook.com/kidstales">Facebook</a> | <a href="http://twitter.com/kidstales">Twitter</a></p>
-			</footer>
 
 		</div> <!-- app div -->
+
+		<footer>
+				<p>Réalisation : étudiants d’Hétic, P2015 | <strong>Dispositions concernant la sécurité des enfants</strong> | <a href="http://facebook.com/kidstales">Facebook</a> | <a href="http://twitter.com/kidstales">Twitter</a></p>
+		</footer>
+
+
 		<div id="map_canvas" <?php if(!$map) echo 'class="hidden"'; ?>></div>
 
 		<div id="popup_overlay"><section id="popup"><div></div></section></div>
