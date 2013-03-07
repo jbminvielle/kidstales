d1ae37896d1c44ba28bb58d355b984ef1c7e9ca5
dyane defoi <dee.defoi@gmail.com>
2013-03-08 00:15:19 +0100

    getmeilleurlieu fonctionne

--- app/Models/Lieu.php
+++ app/Models/Lieu.php
@@ -8,19 +8,18 @@ class Lieu extends Prefab{
         F3::get('db_pw')));
   }
 
-  function getNomLieu($id_hs) {   
+  function getNomLieu($id_hs) {
  
-  return F3::get('dB')->exec(" SELECT lieu.nom from lieu, histoire WHERE id_histoire = ".$id_hs." AND histoire.id_lieu = lieu.id_lieu ");
+    return F3::get('dB')->exec(" SELECT lieu.nom from lieu, histoire WHERE id_histoire = ".$id_hs." AND histoire.id_lieu = lieu.id_lieu ");
 
   }
 
 
   
- function getMeilleursLieu($coords, $limite=10) {
+function getMeilleursLieu() {
 // todo: qd on a cliqué sur le script, la latlgn sont affichés, sur cet event, appeler la bd, selectionner les meilleurs lieux triés par note
-$getMeilleursLieu=new DB\SQL\Mapper(F3::get('dB'),'lieu');
-return$getMeilleursLieu->find(array());
-
+  $getMeilleursLieu=new DB\SQL\Mapper(F3::get('dB'),'lieu');
+  return $getMeilleursLieu->find(null);
 }
 
 
